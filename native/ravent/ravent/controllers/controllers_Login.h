a6c2abc8bc25a07c23491feea74f68f0d24cbcf8
florian haftman <florian.haftman@gmail.com>
2012-04-18 16:43:02 -0700

    splash screen + login animation

---
+++ native/ravent/ravent/controllers/controllers_Login.h
@@ -0,0 +1,13 @@
+//
+//  controllers_Login.h
+//  ravent
+//
+//  Created by florian haftman on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface controllers_Login : UIViewController
+
+@end
