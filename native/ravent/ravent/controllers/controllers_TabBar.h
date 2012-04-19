0a5d6ee34855f4fd34e7f0d90f7344cc6e7a59c5
florian haftman <florian.haftman@gmail.com>
2012-04-19 16:43:59 -0700

    fb login

---
+++ native/ravent/ravent/controllers/controllers_TabBar.h
@@ -0,0 +1,13 @@
+//
+//  controllers_App.h
+//  ravent
+//
+//  Created by florian haftman on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface controllers_App : UITabBarController
+
+@end
---
+++ native/ravent/ravent/controllers/controllers_TabBar.m
@@ -0,0 +1,43 @@
+//
+//  controllers_App.m
+//  ravent
+//
+//  Created by florian haftman on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "controllers_App.h"
+
+@interface controllers_App ()
+
+@end
+
+@implementation controllers_App
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
