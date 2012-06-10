c1af1e9e114f357f9018cc02d191a13add339d5b
Dimitri Rouil <dimitri.rouil@gmail.com>
2012-06-10 00:29:31 -0400

    RAV92

--- service/src/com/chaman/model/Post.java
+++ service/src/com/chaman/model/Post.java
@@ -3,7 +3,6 @@ package com.chaman.model;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.restfb.BinaryAttachment;
 import com.restfb.Connection;
@@ -83,18 +82,10 @@ public class Post  extends Model {
 	public static void ShareEvent(String accessToken, String friendID, String eventID) {
 		
 		FacebookClient client	= new DefaultFacebookClient(accessToken);
-		String msg = "I'm sharing";
-		
-		String query 				= "SELECT uid FROM event_member WHERE eid = " + eventID + " AND uid = " + friendID;
-		List<Attending> friend 	= client.executeQuery(query, Attending.class);
-		
-		if (friend.isEmpty()) { // could also just check the exception return code of the FB call to avoid the fetch
 			
-			client.publish(eventID + "/invited/", FacebookType.class, Parameter.with("users", friendID));
-			msg = msg + " and inviting you to";
-		}
+		client.publish(eventID + "/invited/", Boolean.class, Parameter.with("users", friendID));
 		
-		client.publish(friendID + "/feed", FacebookType.class, Parameter.with("message", msg + " this event " + eventID + (msg.length() > 12 ? "" : " with you") )); //add link;
+		client.publish(friendID + "/feed", FacebookType.class, Parameter.with("message", "I'm inviting you to go to this event #5" + eventID)); //add link;
 	}
 	
 	public String getType() {
