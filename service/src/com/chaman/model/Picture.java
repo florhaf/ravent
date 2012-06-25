4d18531ff2f7b1b39b86e9a0384006b85609fc90
Dimitri Rouil <dimitri.rouil@gmail.com>
2012-06-25 14:13:50 -0400

    Lean picture.class

--- service/src/com/chaman/model/Picture.java
+++ service/src/com/chaman/model/Picture.java
@@ -1,35 +1,14 @@
 package com.chaman.model;
 
 import java.util.ArrayList;
-
-import com.chaman.util.JSON;
 import com.restfb.Connection;
 
 public class Picture extends Model {
 
-	String first_name; // TODO: Change to name
-	String last_name;
-	String picture_user; // of the user
-	String profileID;
-	String time;
-	String url;
 	String picture;
 	
-	public Picture(String accessToken, Post p) {
+	public Picture(Post p) {
 		
-		this.profileID = JSON.GetValueFor("id", p.getFrom());
-		
-		ArrayList<Model> profiles = Profile.GetFacebookProfileInfo(accessToken, this.profileID);
-		
-		if (profiles != null) {
-			
-			Profile profile = (Profile) profiles.get(0);
-			this.first_name = profile.getName();
-			this.picture_user = profile.getPic();
-		}
-		
-		this.time = p.getCreated_time();		
-		this.url = p.getPicture();
 		this.picture = p.getPicture();
 	}
 	
@@ -43,7 +22,7 @@ public class Picture extends Model {
 
 			if (p.getType().equals("photo")) {
 				
-				Picture pic = new Picture(accessToken, p);
+				Picture pic = new Picture(p);
 				result.add(pic);
 			}
 		}
@@ -51,31 +30,7 @@ public class Picture extends Model {
 		return result;
 	}
 	
-	public String getFirst_name() {
-		return this.first_name;
-	}
-	
-	public String getLast_name() {
-		return this.last_name;
-	}
-	
 	public String getPicture() {
 		return this.picture;
-	}
-	
-	public String getTime() {
-		return this.time;
-	}
-	
-	public String getUrl() {
-		return this.url;
-	}
-
-	public String getPicture_user() {
-		return picture_user;
-	}
-
-	public void setPicture_user(String picture_user) {
-		this.picture_user = picture_user;
 	}
 }
