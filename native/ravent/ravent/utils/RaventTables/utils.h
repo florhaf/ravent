56c14ff2270697b73230d90fcb05b8773be98232
florian haftman <florian.haftman@gmail.com>
2012-06-26 19:59:07 -0700

    whole new event details

---
+++ native/ravent/ravent/utils/RaventTables/utils.h
@@ -0,0 +1,13 @@
+//
+//  utils.h
+//  ravent
+//
+//  Created by florian haftman on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface utils : NSObject
+
+@end
---
+++ native/ravent/ravent/utils/RaventTables/utils.m
@@ -0,0 +1,13 @@
+//
+//  utils.m
+//  ravent
+//
+//  Created by florian haftman on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "utils.h"
+
+@implementation utils
+
+@end
