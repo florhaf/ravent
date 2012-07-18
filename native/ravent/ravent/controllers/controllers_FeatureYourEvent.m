bc5ebd95ea661f9fd4520a379acaa3253700d0b4
florian haftman <florian.haftman@gmail.com>
2012-07-17 20:18:26 -0700

    Merge branches 'master' and 'master' of https://github.com/florhaf/ravent

---
+++ native/ravent/ravent/controllers/controllers_FeatureYourEvent.m
@@ -0,0 +1,43 @@
+//
+//  controllers_FeatureYourEvent.m
+//  ravent
+//
+//  Created by florian haftman on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "controllers_FeatureYourEvent.h"
+
+@interface controllers_FeatureYourEvent ()
+
+@end
+
+@implementation controllers_FeatureYourEvent
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
